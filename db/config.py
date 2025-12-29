ea55ee0d6898f32f0709304ac87a90e52b224f30
Andrio Marcos <andriomdac@gmail.com>
2025-12-29 15:21:13 -0300

    my first commit

---
+++ db/config.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+DB_URL = "postgresql://postgres:password@db:5432/postgres"
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
